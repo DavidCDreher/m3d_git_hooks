b01640de65843b403919a89368306adfaeb18060
David Dreher <david.dreher@varian.com>
2019-05-23 16:27:55 +0200

    Rework commit-msg.py to be more reliable and support verbose flag

--- commit-msg.py
+++ commit-msg.py
@@ -7,6 +7,8 @@ import sys
 import os
 import re
 from subprocess import call
+from typing import List, Tuple
+from _io import TextIOWrapper
 
 import hook_utils
 
@@ -16,100 +18,89 @@ else:
     editor = "vim"
 
 
-def check_format_first_line(line, git_comment_char):
-    error_msg = ""
+def marker_at_line_pos(pos: int, end: int, offset: int) -> str:
+    if pos <= offset:
+        ValueError("Invalid starting offset")
+    return f"{' ' * (pos - 1 - offset)}^{'~' * (end - pos - 1)}\n"
+
+
+def check_format_first_line(line: str, git_comment_char: str) -> List[str]:
+    messages = []
     if not re.match(hook_utils.COMMIT_MSG_RE, line):
-        msg = (
-                git_comment_char
-                + " Error: Subject line should start with a #(ISSUE) or with 'Merge'\n"
+        messages.append(
+            f"{git_comment_char} Error: Subject line should start with a "
+            f"#(ISSUE) or with 'Merge'\n"
         )
-        error_msg = error_msg + msg
         issue_match = re.match(hook_utils.ISSUE_NUMBER_RE, line)
-        issue_branch_number, number_msg = hook_utils.get_issue_num_by_branch()
-        if number_msg:
-            error_msg += f"{git_comment_char} {number_msg}"
+        issue_branch_number, _ = hook_utils.get_issue_num_by_branch()
         if issue_match and issue_branch_number:
             if issue_match.group(1) != issue_branch_number:
-                msg = (
-                        git_comment_char
-                        + " Error: Mismatch between branch issue number and issue number in commit message\n"
+                messages.append(
+                    f"{git_comment_char} Error: Mismatch between branch issue "
+                    f"number and issue number in commit message\n"
                 )
-                error_msg = error_msg + msg
     if len(line) > 50:
-        msg = git_comment_char + " Error: Limit the subject line to 50 characters"
-        error_msg = (
-            error_msg
-            + msg
-            + (" " * (50 - 1 - len(msg)))
-            + "^"
-            + ("~" * (len(line) - 50 - 1))
-            + "\n"
-        )
+        msg = f"{git_comment_char} Error: Limit the subject line to 50 characters"
+        messages.append(msg + marker_at_line_pos(50, len(line), len(msg)))
     first_word_re = "[^a-zA-Z]*([a-zA-Z]+).*"
     first_word_match = re.match(first_word_re, line)
     if first_word_match:
         first_word = first_word_match.group(1)
         if first_word[0].islower():
-            msg = git_comment_char + " Error: Capitalize the subject line\n"
-            error_msg = error_msg + msg
+            messages.append(f"{git_comment_char} Error: Capitalize the subject line\n")
     else:
-        msg = git_comment_char + " Error: Subject line should include a summary\n"
-        error_msg = error_msg + msg
+        messages.append(
+            f"{git_comment_char} Error: Subject line should include a summary\n"
+        )
     if line and line[-1] == ".":
-        msg = git_comment_char + " Error: Do not end the subject line with a period\n"
-        error_msg = error_msg + msg
-    return error_msg
+        messages.append(
+            f"{git_comment_char} Error: Do not end the subject line with a period\n"
+        )
+    return messages
 
 
-def check_format_rules(line_num, line, git_comment_char):
-    if line_num == 0:
-        return check_format_first_line(line, git_comment_char)
-    if line_num == 1:
-        if line:
-            return git_comment_char + " Error: Second line ^ should be empty.\n"
-    if not line.startswith(git_comment_char):
-        if len(line) > 80:
-            err_msg = git_comment_char + " Error: No line should exceed 80 characters:"
-            err_msg = (
-                err_msg
-                + (" " * (80 - 1 - len(err_msg)))
-                + "^"
-                + ("~" * (len(line) - 80 - 1))
-                + "\n"
-            )
-            return err_msg
-    return False
+def parse_commit_message(commit_file_object: TextIOWrapper) -> Tuple[List[str], bool]:
+    comment_char, _ = hook_utils.get_git_comment_char()
+    reached_verbose_body = False
+    mod_commit_msg = []
+    line_number = -1
+    contains_errors = False
+    for line in commit_file_object:
+        if hook_utils.GIT_VERBOSE_HEADER in line:
+            reached_verbose_body = True
+        if reached_verbose_body:
+            mod_commit_msg.append(line)
+            continue
+        if not line.startswith(f"{comment_char} Error: "):
+            line_number += 1
+            mod_commit_msg.append(line)
+            if line_number == 0:
+                err_msgs = check_format_first_line(line, comment_char)
+                if err_msgs:
+                    mod_commit_msg.extend(err_msgs)
+                    contains_errors = True
+            elif not line.startswith(comment_char):
+                if line_number == 1 and line.strip():
+                    mod_commit_msg.append(
+                        f"{comment_char} Error: Second line ^ should be empty.\n"
+                    )
+                    contains_errors = True
+                elif len(line) > 80:
+                    msg = f"{comment_char} Error: No line should exceed 80 characters:"
+                    mod_commit_msg.append(msg + marker_at_line_pos(80, len(line), len(msg)))
+                    contains_errors = True
+    return mod_commit_msg, contains_errors
 
 
 if __name__ == "__main__":
     message_file = sys.argv[1]
     try:
-        comment_char, comment_msg = hook_utils.get_git_comment_char()
         while True:
-            new_commit_msg = list()
-            errors = False
             with open(message_file) as commit_fd:
-                line_number = -1
-                for curr_line in commit_fd:
-                    stripped_line = curr_line.strip()
-                    # Stop reading in case of verbose mode when encountering header
-                    if stripped_line.startswith(comment_char + hook_utils.GIT_VERBOSE_HEADER):
-                        break
-                    # Read only non-comment lines
-                    if not stripped_line.startswith(comment_char):
-                        line_number += 1
-                        new_commit_msg.append(curr_line)
-                        e = check_format_rules(line_number, stripped_line, comment_char)
-                        if (line_number == 0) and comment_msg:
-                            e += f"{comment_char} {comment_msg}"
-                        if e:
-                            new_commit_msg.append(e)
-                            errors = True
-            if errors:
+                commit_msg, has_errors = parse_commit_message(commit_fd)
+            if has_errors:
                 with open(message_file, "w") as commit_fd:
-                    for curr_line in new_commit_msg:
-                        commit_fd.write(curr_line)
-                    commit_fd.write("\n")
+                    commit_fd.writelines(commit_msg)
                 user_msg = "Errors in commit message format. "
                 user_options = ["Abort", "edit", "ignore"]
                 user_choice = hook_utils.ask_user_choice(user_msg, user_options)
